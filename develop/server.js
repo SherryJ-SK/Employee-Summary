9e35f1d173a4fd1d944ad92783e17b00641d8051
Sherry Jin <a-sherry@live.com>
2020-01-29 00:20:14 +1100

    install npm

---
+++ develop/server.js
@@ -0,0 +1,19 @@
+const inquirer = require("inquirer");
+
+// prompt questions ask to create manager
+const questions = [
+    {
+        type: "input",
+        name: "manager",
+        message: "Enter manager information"
+    }
+]
+// manager email
+// prompt questions ask engineer or intern
+// engineer ask for github username and email
+// intern ask for email and school
+
+// give IDs to employees based on the order
+
+// apply the info into html
+
