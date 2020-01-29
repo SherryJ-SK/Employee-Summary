f86c9e7158a20f7609b1868da677530a7965aff1
Sherry <a-sherry@live.com>
2020-01-29 23:08:12 +1100

    suodo coding

---
+++ develop/Manager.js
@@ -0,0 +1,9 @@
+class Manager {
+    constructor(id, role, officeNumber) {
+        this.id = id;
+        this.role = role;
+        this.officeNumber = officeNumber;
+    }
+} 
+
+module.exports = Manager;
