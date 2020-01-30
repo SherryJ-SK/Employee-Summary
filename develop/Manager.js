6d75b6a3c633a7601c7576b641ba3039999591a0
Sherry Jin <a-sherry@live.com>
2020-01-30 23:35:28 +1100

    employee and manager js

--- develop/Manager.js
+++
@@ -1,9 +0,0 @@
-class Manager {
-    constructor(id, role, officeNumber) {
-        this.id = id;
-        this.role = role;
-        this.officeNumber = officeNumber;
-    }
-} 
-
-module.exports = Manager;
