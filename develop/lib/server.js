eb10921dd0a33fdac6310b529bde9eb893487788
Sherry Jin <a-sherry@live.com>
2020-02-01 00:37:13 +1100

    prompt question

--- develop/lib/server.js
+++ develop/lib/server.js
@@ -1,71 +1,50 @@
 const inquirer = require("inquirer");
-const axios = require("axios");
-const fs = require("fs");
-const Employee = require("Employee");
-const Manager = require("Manager");
-const Engineer = require("Engineer");
-const Intern = require("intern");
+// const axios = require("axios");
+// const fs = require("fs");
+// const Employee = require("./Employee");
+const Manager = require("./Manager");
+// const Engineer = require("./Engineer");
+// const Intern = require("./intern");
 
-
-
+// const employees = [];
+// const manager = [];
+// const engineer = [];
+// const intern = [];
 
 const questionManager = [
     {
-        type: "confirm",
-        name: "managerConfirm",
-        message: "Would you like to enter manager information?",
-    }
-];
-
-const questionManagerContinue = [
+        type: "input",
+        name: "name",
+        message: "Enter manager's name",
+    },
     {
         type: "input",
-        name: "manager",
-        message: "Please enter the manager's email: "
+        name: "email",
+        message: "Enter manager's email",
+    },
+    {
+        type: "input",
+        name: "officeNumber",
+        message: "Enter manager's office number",
     }
 ];
 
-const employee = [{
-    manager: [{}],
-    engineer: [{}],
-    intern: [{}]
-}];
+inquirer.prompt(questionManager)
+    .then(function (data) {
+        Manager.getName(data.name);
+        Manager.getId();
+        Manager.getEmail(data.email);
+        Manager.getOfficeNumber(data.officeNumber);
+        Manager.getRole();
+    })
 
-
-
-class Team {
-    
     // prompt questions ask to create manager
-
     // if yes, ask for manager information;
     // prompt questions ask engineer or intern
     // engineer ask for github username and email
     // intern ask for email and school
-
     // give IDs to employees based on the order
-
     // apply the info into html
+    // if yes, ask for manager information;
+    // if no, "has to enter manager info", quit
 
-
-    // askForManager() {
-    //     inquirer
-    //         .prompt(questionManager)
-    //         .then(function choice() {
-    //             // if yes, ask for manager information;
-    //             if (choice.managerConfirm) {
-    //                 managerInfo();
-    //             } else {
-    //                 // if no, "has to enter manager info", quit
-    //                 console.log("Manager information has to be entered");
-    //             }
-    //         })
-    // };
-
-    // managerInfo() {
-    //     inquirer.prompt(questionManagerContinue)
-    //         .then(
-
-    //         )
-    // };
-
-}
